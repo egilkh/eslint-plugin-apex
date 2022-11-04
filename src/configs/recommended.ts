c58f5039d5fad716a2297eb8f0da27658555b3b9
sketch <foo-bar-sketch@protonmail.com>
2022-11-04 21:21:08 +0100

    build: removes rules provided by other package

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -4,9 +4,6 @@ export = {
   rules: {
     //  "nestjs-typed/api-methods-have-documentation": "error",
     'nestjs-apex/api-method-should-specify-api-response': 'error',
-    'nestjs-apex/validated-non-primitive-property-needs-type-decorator':
-      'error',
-    'nestjs-apex/all-properties-are-whitelisted': 'error',
     'nestjs-apex/class-named-service-should-have-injectable-decorator': 'error',
     'nestjs-apex/class-named-guard-should-have-injectable-decorator': 'error',
   },
