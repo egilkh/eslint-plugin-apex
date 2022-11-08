8864e4b4380de9903a72567b758de2565f3cc4c0
foo-bar-sketch <42802826+foo-bar-sketch@users.noreply.github.com>
2022-11-08 08:08:05 +0100

    build: removes rules provided by other package (#2)

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,14 +1,9 @@
-import allPropertiesAreWhitelisted from './all-properties-are-whitelisted/allPropertiesAreWhitelisted';
 import apiMethodsShouldSpecifyApiResponse from './api-method-should-specify-api-response/apiMethodsShouldSpecifyApiResponse';
 import classNamedGuardShouldHaveInjectableDecorator from './class-named-guard-should-have-injectable-decorator/classNamedGuardShouldHaveInjectableDecorator';
 import classNamedServiceShouldHaveInjectableDecorator from './class-named-service-should-have-injectable-decorator/classNamedServiceShouldHaveInjectableDecorator';
-import validateNonPrimitiveNeedsDecorators from './validated-non-primitive-property-needs-type-decorator/validateNonPrimitiveNeedsDecorators';
 
 const allRules = {
   'api-method-should-specify-api-response': apiMethodsShouldSpecifyApiResponse,
-  'all-properties-are-whitelisted': allPropertiesAreWhitelisted,
-  'validated-non-primitive-property-needs-type-decorator':
-    validateNonPrimitiveNeedsDecorators,
   'class-named-service-should-have-injectable-decorator':
     classNamedServiceShouldHaveInjectableDecorator,
   'class-named-guard-should-have-injectable-decorator':
