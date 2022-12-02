f6234aa5e9d92b48d97fc87769ef4e549368f8be
Daniel <foo-bar-sketch@protonmail.com>
2022-12-02 14:42:33 +0100

    feat(rules): adds transaction must be commited and rolled back rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,7 @@
 import apiMethodsShouldSpecifyApiResponse from './api-method-should-specify-api-response/apiMethodsShouldSpecifyApiResponse';
 import classNamedGuardShouldHaveInjectableDecorator from './class-named-guard-should-have-injectable-decorator/classNamedGuardShouldHaveInjectableDecorator';
 import classNamedServiceShouldHaveInjectableDecorator from './class-named-service-should-have-injectable-decorator/classNamedServiceShouldHaveInjectableDecorator';
+import transactionsMustBeCommitted from './transactions-must-be-committed/transactionsMustBeCommitted';
 
 const allRules = {
   'api-method-should-specify-api-response': apiMethodsShouldSpecifyApiResponse,
@@ -8,6 +9,7 @@ const allRules = {
     classNamedServiceShouldHaveInjectableDecorator,
   'class-named-guard-should-have-injectable-decorator':
     classNamedGuardShouldHaveInjectableDecorator,
+  'transactions-must-be-committed': transactionsMustBeCommitted,
 };
 
 export default allRules;
