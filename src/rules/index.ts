6f3a20c4461df706c57d0c78ebd9643db706a726
foo-bar-sketch <42802826+foo-bar-sketch@users.noreply.github.com>
2022-12-05 14:08:33 +0100

    feat(sequelize-transactions-must-be-committed): adds rule (#3)

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,7 @@
 import apiMethodsShouldSpecifyApiResponse from './api-method-should-specify-api-response/apiMethodsShouldSpecifyApiResponse';
 import classNamedGuardShouldHaveInjectableDecorator from './class-named-guard-should-have-injectable-decorator/classNamedGuardShouldHaveInjectableDecorator';
 import classNamedServiceShouldHaveInjectableDecorator from './class-named-service-should-have-injectable-decorator/classNamedServiceShouldHaveInjectableDecorator';
+import sequelizeTransactionsMustBeCommitted from './transactions-must-be-committed/sequelizeTransactionsMustBeCommitted';
 
 const allRules = {
   'api-method-should-specify-api-response': apiMethodsShouldSpecifyApiResponse,
@@ -8,6 +9,7 @@ const allRules = {
     classNamedServiceShouldHaveInjectableDecorator,
   'class-named-guard-should-have-injectable-decorator':
     classNamedGuardShouldHaveInjectableDecorator,
+  'transactions-must-be-committed': sequelizeTransactionsMustBeCommitted,
 };
 
 export default allRules;
